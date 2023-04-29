6461a3848d9a85ac7c947ff4b129747f2fe6ec57
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-29 21:37:23 +0300

    ettend ExceptionHandling

--- src/main/java/com/example/supportportal/controller/UserController.java
+++ src/main/java/com/example/supportportal/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.example.supportportal.controller;
 
+import com.example.supportportal.exception.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UserController extends ExceptionHandling {
 
 
     @GetMapping("/test")
