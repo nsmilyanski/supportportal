7b67f70a1bae1aab7b60c91837cb71568fec2c13
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-30 22:15:01 +0300

    Add Role enum

---
+++ src/main/java/com/example/supportportal/enumeration/Role.java
@@ -0,0 +1,20 @@
+package com.example.supportportal.enumeration;
+
+import lombok.Getter;
+
+import static com.example.supportportal.constant.Authority.*;
+
+@Getter
+public enum Role {
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+
+    private String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+}
