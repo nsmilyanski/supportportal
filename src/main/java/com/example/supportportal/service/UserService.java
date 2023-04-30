2ec4d45638a9381fdcc99f7e208c50312d1f53e2
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-30 22:15:23 +0300

    Add user methods

--- src/main/java/com/example/supportportal/service/UserService.java
+++ src/main/java/com/example/supportportal/service/UserService.java
@@ -1,4 +1,18 @@
 package com.example.supportportal.service;
 
+
+import com.example.supportportal.exception.domain.EmailExistException;
+import com.example.supportportal.exception.domain.UserNotFoundException;
+import com.example.supportportal.exception.domain.UsernameExistException;
+import com.example.supportportal.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, EmailExistException, UsernameExistException;
+
+    List<User> getUsers();
+
+    User findUserByUsername(String username);
+    User findUserByEmail(String email);
 }
