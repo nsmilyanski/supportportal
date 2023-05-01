4eb3b13cec7f67914d2595a026dc6468c6170b16
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-01 21:13:08 +0300

    Add user impl constants

---
+++ src/main/java/com/example/supportportal/constant/UserImplConstant.java
@@ -0,0 +1,8 @@
+package com.example.supportportal.constant;
+
+public class UserImplConstant {
+    public static final String EMAIL_ALREADY_EXIST = "Email already exist!";
+    public static final String USERNAME_ALREADY_EXIST = "Username already exist!";
+    public static final String NO_USER_FOUND_BY_USERNAME = "No user found by username: ";
+    public static final String USER_IMAGE_PROFILE_TEMP = "/user/image/profile/temp";
+}
