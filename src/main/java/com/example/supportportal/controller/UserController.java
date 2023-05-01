09bbf60a7389efab9b9783348b6d60fa9b4c2219
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-01 21:13:42 +0300

    Add login and register endpoint

--- src/main/java/com/example/supportportal/controller/UserController.java
+++ src/main/java/com/example/supportportal/controller/UserController.java
@@ -1,18 +1,58 @@
 package com.example.supportportal.controller;
 
 import com.example.supportportal.exception.ExceptionHandling;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.supportportal.exception.domain.EmailExistException;
+import com.example.supportportal.exception.domain.UserNotFoundException;
+import com.example.supportportal.exception.domain.UsernameExistException;
+import com.example.supportportal.model.User;
+import com.example.supportportal.model.UserPrincipal;
+import com.example.supportportal.service.UserService;
+import com.example.supportportal.utility.JWTTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.example.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping( path = {"/","/users"})
+@RequiredArgsConstructor
 public class UserController extends ExceptionHandling {
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JWTTokenProvider tokenProvider;
 
 
-    @GetMapping("/test")
-    public String getFromSecureEndpoint() {
-        return "Hi from Spring Security";
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException {
+        return new ResponseEntity<>(userService.register(user.getFirstName(), user.getLastName(), user.getUsername(),user.getEmail()), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException {
+        authenticate(user.getEmail(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JWT_TOKEN_HEADER,tokenProvider.generateJwtToken(userPrincipal) );
+        return httpHeaders;
+    }
+
+    private void authenticate(String email, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
     }
 
 }
