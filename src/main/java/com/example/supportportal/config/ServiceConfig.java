d5f4866db9014ced91d744a0a1e8e23939318f22
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-02 22:07:50 +0300

    add service config

---
+++ src/main/java/com/example/supportportal/config/ServiceConfig.java
@@ -0,0 +1,23 @@
+package com.example.supportportal.config;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class ServiceConfig {
+    @Bean
+    public LoadingCache<String, Integer> loadingCache() {
+        return CacheBuilder.newBuilder().expireAfterWrite(15, TimeUnit.MINUTES).maximumSize(100)
+                .build(new CacheLoader<String, Integer>() {
+                    @Override
+                    public Integer load(String key) throws Exception {
+                        return 0;
+                    }
+                });
+    }
+}
