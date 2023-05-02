691ce37163c14f8eaf2ebbc3a24f3c95966addf9
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-02 22:09:01 +0300

    add user validation for vrute force attack

--- src/main/java/com/example/supportportal/config/ApplicationConfig.java
+++ src/main/java/com/example/supportportal/config/ApplicationConfig.java
@@ -3,7 +3,9 @@ package com.example.supportportal.config;
 import com.example.supportportal.model.User;
 import com.example.supportportal.model.UserPrincipal;
 import com.example.supportportal.repository.UserRepository;
+import com.example.supportportal.service.LoginAttemptService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,12 +17,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class ApplicationConfig {
     private final UserRepository userRepository;
+    private final LoginAttemptService loginAttemptService;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -28,9 +33,17 @@ public class ApplicationConfig {
             Optional<User> userByEmail = userRepository.findUserByEmail(username);
 
             if (!userByEmail.isPresent()) {
-                throw new UsernameNotFoundException( String.format("User not found by username %s", username));
+                throw new UsernameNotFoundException( String.format("User not found by email %s", username));
             }
-            return new UserPrincipal(userByEmail.get());
+            User user = userByEmail.get();
+
+            validateLoginAttempt(user);
+
+            user.setLastLoginDateDisplay(user.getLastLoginDateDisplay());
+            user.setLastLoginDate(new Date());
+
+            userRepository.save(user);
+            return new UserPrincipal(user);
         };
     }
 
@@ -50,5 +63,13 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private void validateLoginAttempt(User user)  {
+        if (user.isNotLocked()) {
+            user.setNotLocked(!loginAttemptService.hasExceededMacAttempts(user.getEmail()));
+        }else {
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getEmail());
+        }
     }
 }
