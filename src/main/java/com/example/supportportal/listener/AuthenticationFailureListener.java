30b7363e03ef27bddebfeb0d5394e05138ece2d4
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-02 22:09:50 +0300

    add AuthenticationFailureListener

---
+++ src/main/java/com/example/supportportal/listener/AuthenticationFailureListener.java
@@ -0,0 +1,26 @@
+package com.example.supportportal.listener;
+
+import com.example.supportportal.service.LoginAttemptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFailureListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+
+        if (principal instanceof String) {
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+    }
+}
