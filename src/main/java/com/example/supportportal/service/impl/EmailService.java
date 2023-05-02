9b252a408b8ecaa5d240c75c154440144d0d957d
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-02 22:07:36 +0300

    Add email service

---
+++ src/main/java/com/example/supportportal/service/impl/EmailService.java
@@ -0,0 +1,41 @@
+package com.example.supportportal.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+import static com.example.supportportal.constant.EmailConstant.*;
+import static javax.mail.Message.RecipientType.CC;
+import static javax.mail.Message.RecipientType.TO;
+
+@Service
+public class EmailService {
+    private Message createEmail(String firstName, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(TO, InternetAddress.parse(email, false));
+        message.setRecipients(CC, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello " + firstName + ", \n \n Your new account password is: " + password + "\n \n The Support Team");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+    }
+
+    private Session getEmailSession() {
+        Properties properties = System.getProperties();
+        properties.put(SMTP_HOST, GMAIL_SMTP_SERVER);
+        properties.put(SMTP_AUTH, true);
+        properties.put(SMTP_PORT, DEFAULT_PORT);
+        properties.put(SMTP_STARTTLS_ENABLE, true);
+        properties.put(SMTP_STARTTLS_REQUIRED, true);
+
+        return Session.getInstance(properties, null);
+    }
+}
