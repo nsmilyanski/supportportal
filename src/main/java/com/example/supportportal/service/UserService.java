40b022b1516a94e394e1ce617d2f3ef090cfdc0d
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-03 16:57:58 +0300

    Add abstract methods

--- src/main/java/com/example/supportportal/service/UserService.java
+++ src/main/java/com/example/supportportal/service/UserService.java
@@ -2,10 +2,13 @@ package com.example.supportportal.service;
 
 
 import com.example.supportportal.exception.domain.EmailExistException;
+import com.example.supportportal.exception.domain.EmailNotFoundException;
 import com.example.supportportal.exception.domain.UserNotFoundException;
 import com.example.supportportal.exception.domain.UsernameExistException;
 import com.example.supportportal.model.User;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -15,4 +18,13 @@ public interface UserService {
 
     User findUserByUsername(String username);
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNotLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
+    User updateUser(String currentUsername,String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNotLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
+
+    void deleteUser(Integer id);
+
+    void resetPassword(String email) throws EmailNotFoundException;
+
+    User updateProfileImage(String username, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
 }
