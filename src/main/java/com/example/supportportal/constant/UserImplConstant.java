de9a4829087a41ec32a1659efd32b8e71f6f4060
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-03 16:58:47 +0300

    add NO_USER_FOUND_BY_EMAIL constant

--- src/main/java/com/example/supportportal/constant/UserImplConstant.java
+++ src/main/java/com/example/supportportal/constant/UserImplConstant.java
@@ -4,5 +4,6 @@ public class UserImplConstant {
     public static final String EMAIL_ALREADY_EXIST = "Email already exist!";
     public static final String USERNAME_ALREADY_EXIST = "Username already exist!";
     public static final String NO_USER_FOUND_BY_USERNAME = "No user found by username: ";
+    public static final String NO_USER_FOUND_BY_EMAIL = "No user found by email: ";
     public static final String USER_IMAGE_PROFILE_TEMP = "/user/image/profile/temp";
 }
