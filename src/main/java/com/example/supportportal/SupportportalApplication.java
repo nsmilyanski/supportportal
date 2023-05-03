58eacc1fb9a58f9a5ebd8699f9bfd71cc7fc257b
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-03 16:58:36 +0300

    create directory for users

--- src/main/java/com/example/supportportal/SupportportalApplication.java
+++ src/main/java/com/example/supportportal/SupportportalApplication.java
@@ -3,11 +3,16 @@ package com.example.supportportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
+import static com.example.supportportal.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class SupportportalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SupportportalApplication.class, args);
+        new File(USER_FOLDER).mkdirs();
     }
 
 }
