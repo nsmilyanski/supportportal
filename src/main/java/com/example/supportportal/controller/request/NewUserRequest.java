114416e5d58f0e950a143b113cbca5967f9e41f0
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-04 13:39:37 +0300

    implemnt NewUserRequest class

--- src/main/java/com/example/supportportal/controller/request/NewUserRequest.java
+++ src/main/java/com/example/supportportal/controller/request/NewUserRequest.java
@@ -1,2 +1,43 @@
-package com.example.supportportal.controller.request;public class NewUserRequest {
+package com.example.supportportal.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewUserRequest {
+
+    private String currentUsername;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String role;
+
+    @NotNull
+    private boolean isNotLocked;
+
+    @NotNull
+    private boolean active;
+
+    MultipartFile profileImage;
 }
